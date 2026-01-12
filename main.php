083e2d0f5c085ed32185beffa3ce85a8143ec978
ghost-mann <iamaob8@protonmail.com>
2026-01-12 10:32:18 +0300

    feat: added main.php a refined version of crypto.php

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cryptocurrency Prices</title>
+</head>
+<body>
+    <h1>Latest Cryptocurrency Prices</h1>
+    <table>
+        <th>COIN</th>
+        <th>Price(USD)</th>
+    </table>
+</body>
+
+</html>
