49eca0c98b30da8e3eda4fdaa2bdf7a3e93198c3
ghost-mann <iamaob8@protonmail.com>
2026-01-12 09:19:11 +0300

    featL added styling in styles.css

--- crypto.php
+++ crypto.php
@@ -6,9 +6,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Crypto Prices</title>
 </head>
 <body>
-
+    <div class="container">
+        <h1>Latest Crypto Prices</h1>
+    </div>
 </body>
 </html>
